a038ab5d0b0822704f49d38f5f3d4e9d3dd6dc8d
Christian Rebelsky <christian.rebelsky@gmail.com>
2018-03-16 23:48:29 +0100

    add set of configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
 module.exports = {
+  pathPrefix: '/',
   siteMetadata: {
-    title: 'Gatsby Strict Starter Next',
+    title: 'Balz und Balz',
+    siteUrl: 'http://balzundbalz.de/',
   },
   plugins: [
     'gatsby-plugin-react-next',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
+    'gatsby-source-filesystem',
+    'gatsby-plugin-manifest',
+    'gatsby-image',
+    'gatsby-plugin-canonical-urls',
+    'gatsby-plugin-google-analytics',
+    'gatsby-plugin-manifest',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-sitemap',
+    'gatsby-transformer-json',
+    'gatsby-plugin-react-svg',
   ],
 };
